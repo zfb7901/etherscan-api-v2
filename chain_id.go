7d8c26cf0a4b704e5bd70e6f621ca38165385e10
zfb7901 <zfb7901@163.com>
2025-06-15 23:02:41 +0800

    revize
    Signed-off-by: zfb7901 <zfb7901@163.com>

--- chain_id.go
+++ chain_id.go
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2018 LI Zhennan
- *
+ * 
  * Use of this work is governed by a MIT License.
  * You may find a license copy in project root.
  */
--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/zfb7901/etherscan-api-v2
 
-go 1.23.6
+go 1.23.6 
 
 require github.com/google/go-cmp v0.7.0 // indirect
